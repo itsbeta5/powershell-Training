124a61476dd8654e0f7dee39ea49a4c0f254b5d1
Ibrahim Saeid Beta <78052520+itsbeta5@users.noreply.github.com>
2024-02-26 06:09:58 +0200

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,3 +1,1 @@
-1..1024 | ForEach-Object { $sock = New-Object System.Net.Sockets.TcpClient; $async =
-$sock.BeginConnect('localhost', $_, $null, $null); $wait = $async.AsyncWaitHandle.WaitOne(100, $false);
-if($sock.Connected) { $_ } ; $sock.Close() }
+Write-Host "Hello World!"
